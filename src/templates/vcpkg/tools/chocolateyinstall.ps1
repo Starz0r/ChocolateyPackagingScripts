9cb46212701ab796741158aae2c94802e436650d
Starz0r <starz0r@starz0r.com>
2020-03-05 20:37:46 -0600

    vcpkg: silently continue when calling git

--- src/templates/vcpkg/tools/chocolateyinstall.ps1
+++ src/templates/vcpkg/tools/chocolateyinstall.ps1
@@ -1,8 +1,9 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'SilentlyContinue';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installDir = Join-Path $(Get-ToolsLocation) $PackageName
 
 git clone --single-branch --branch $tag https://github.com/Microsoft/vcpkg.git $installDir
 Invoke-BatchFile "$installDir\bootstrap-vcpkg.bat"
+$ErrorActionPreference = 'Stop';
 Install-BinFile -Name "vcpkg" -Path "$installDir\vcpkg.exe"
 & "$installDir\vcpkg.exe" integrate install
