69459252f831159f09c842108a25cb8b2d0a5573
Starz0r <starz0r@starz0r.com>
2020-03-05 20:48:46 -0600

    vcpkg: declare each step, conditionals for coming from an upgrade

--- src/templates/vcpkg/tools/chocolateyinstall.ps1
+++ src/templates/vcpkg/tools/chocolateyinstall.ps1
@@ -2,8 +2,28 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $installDir = Join-Path $(Get-ToolsLocation) $PackageName
 
-git clone --single-branch --branch $tag https://github.com/Microsoft/vcpkg.git $installDir
-Invoke-BatchFile "$installDir\bootstrap-vcpkg.bat"
+$preinstalled = Test-Path -Path $installDir -PathType Container
+$precompiled = Test-Path -Path "$installDir\vcpkg.exe" -PathType Leaf
+
+if ($preinstalled) {
+	Write-Host -ForegroundColor green "Vcpkg repository already exists, pulling new changes instead."
+	git pull -C $installDir
+} else {
+	Write-Host -ForegroundColor green "Cloning GitHub Repository"
+	git clone --single-branch --branch $tag https://github.com/Microsoft/vcpkg.git $installDir
+}
+
+if ($precompiled) {
+	Write-Host -ForegroundColor green "Vcpkg already compiled, deleting executable."
+	Remove-Item -Path "$installDir\vcpkg.exe"
+}
+
 $ErrorActionPreference = 'Stop';
+Write-Host -ForegroundColor green "Bootstraping vcpkg"
+Invoke-Expression "$installDir\bootstrap-vcpkg.bat"
+
+Write-Host -ForegroundColor green "Adding Shim to PATH"
 Install-BinFile -Name "vcpkg" -Path "$installDir\vcpkg.exe"
-& "$installDir\vcpkg.exe" integrate install
+
+Write-Host -ForegroundColor green "Setting vcpkg root"
+Invoke-Expression "$installDir\vcpkg.exe integrate install"
