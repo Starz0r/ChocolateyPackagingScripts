3b4acb15555cbe188c604c26db66d3377ff4c1aa
Starz0r <starz0r@starz0r.com>
2020-05-17 18:31:08 -0500

    rust-analyzer: don't manually check processor width
    Helper function can block out 32-bit users by just not specifying normal 32-bit parameters and only 64-bit ones.

--- src/templates/rust-analyzer/tools/chocolateyinstall.ps1
+++ src/templates/rust-analyzer/tools/chocolateyinstall.ps1
@@ -1,10 +1,4 @@
 ﻿$ErrorActionPreference = "Stop";
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-if (Get-OSArchitectureWidth 32) {
-	Write-Host -ForegroundColor red "Unsupported 32-Bit System."
-	exit 1
-}
-
-Get-ChocolateyWebFile -PackageName "rust-analyzer" -FileFullPath "$installDir\rust-analyzer.exe" -Url "$url"
-Get-ChecksumValid -File "$installDir\rust-analyzer.exe" -Checksum "$checksum" -ChecksumType "SHA512" -OriginalUrl "$url"
+Get-ChocolateyWebFile -PackageName "rust-analyzer" -FileFullPath "$installDir\rust-analyzer.exe" -Url64bit "$url" -Checksum64 "$checksum" -ChecksumType64 "SHA512"
