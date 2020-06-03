864c6daba9254d81e23c7f9a700187778205edb7
Starz0r <starz0r@starz0r.com>
2020-06-03 14:15:19 -0500

    tweetduck.install: nupkg template

---
+++ src/templates/tweetduck.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,41 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName  = $env:ChocolateyPackageName
+$installDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32        = "$url"
+
+$stop_application = if (Get-Process -Name TweetDuck -ErrorAction SilentlyContinue) {$false} else {$true}
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $installDir
+  fileType      = 'exe'
+  url           = $url32
+
+  softwareName  = 'tweetduck*'
+
+  checksum      = "$checksum"
+  checksumType  = 'sha512'
+  
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+
+if ($stop_application -and (Get-Process -Name TweetDuck -ErrorAction SilentlyContinue)) {
+    Stop-Process -processname TweetDuck
+}
+
+Install-ChocolateyPackage @packageArgs
+$installLocation = Get-AppInstallLocation "$packageName*"
+
+Install-BinFile $packageName "$installLocation\$packageName.exe"
+Register-Application "$installLocation\$packageName.exe"
+Write-Host "$packageName registered as $packageName"
+
+
+
+
+
+
+    
---
+++ src/templates/tweetduck.install/tools/chocolateyuninstall.ps1
@@ -0,0 +1,28 @@
+$ErrorActionPreference = 'Stop'
+
+$packageName = $env:ChocolateyPackageName
+
+Uninstall-BinFile $packageName
+
+[array] $key = Get-UninstallRegistryKey "packageName*"
+if ($key.Count -eq 1) {
+    $key | ForEach-Object {
+        $packageArgs = @{
+            packageName            = $packageName
+            silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+            fileType               = 'EXE'
+            validExitCodes         = @(0)
+            file                   = "$($_.UninstallString.Replace(' /x86=0', ''))"
+        }
+        Uninstall-ChocolateyPackage @packageArgs
+    }
+}
+elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+    Write-Warning "$($key.Count) matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
+}
