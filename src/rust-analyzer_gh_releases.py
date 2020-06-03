2f25c4eea91e95da7791b6a2805ed2ecfe6689df
Starz0r <starz0r@starz0r.com>
2020-06-03 14:18:02 -0500

    rust-analyzer: move script over to common functions framework

--- src/rust-analyzer_gh_releases.py
+++ src/rust-analyzer_gh_releases.py
@@ -5,6 +5,11 @@ import subprocess
 import checksum
 from pathlib import Path
 from string import Template
+
+from common.common import find_and_replace_templates
+from common.common import abort_on_nonzero
+from common.common import get_correct_release_asset
+
 from github import Github
 
 
@@ -16,7 +21,7 @@ def main():
         for _, line in enumerate(f):
             state.append(line)
     f = io.open("rust-analyzer.ghstate", "a+")
-    # connect to github api 
+    # connect to github api
     gh = Github(os.environ['GH_TOKEN'])
     rustlsp = gh.get_repo("rust-analyzer/rust-analyzer")
     for rel in rustlsp.get_releases():
@@ -28,7 +33,12 @@ def main():
             else:
                 continue
         if not pushed:
-            url = get_correct_release_asset(rel.get_assets())
+            asset = get_correct_release_asset(rel.get_assets(),
+                                              "windows",
+                                              None)
+            if asset is None:
+                continue
+            url = asset.browser_download_url
             subprocess.call(["wget",
                              url,
                              "--output-document",
@@ -38,19 +48,31 @@ def main():
             tempdir = tempfile.mkdtemp()
             if "nightly" in rel.title:
                 t = rel.published_at
-                find_and_replace_templates(tempdir,
+                find_and_replace_templates("rust-analyzer",
+                                           tempdir,
                                            "{}.{}.{}-nightly".format(t.year,
                                                                      t.month,
                                                                      t.day),
                                            rel.tag_name,
                                            url,
-                                           chksum)
+                                           chksum,
+                                           None,
+                                           None,
+                                           None,
+                                           None,
+                                           None)
             else:
-                find_and_replace_templates(tempdir,
+                find_and_replace_templates("rust-analyzer",
+                                           tempdir,
                                            rel.title.replace("-", "."),
                                            rel.tag_name,
                                            url,
-                                           chksum)
+                                           chksum,
+                                           None,
+                                           None,
+                                           None,
+                                           None,
+                                           None)
             abort_on_nonzero(subprocess.call(["choco",
                                               "pack",
                                               Path(tempdir)/"rust-analyzer.nuspec"]))
@@ -60,35 +82,6 @@ def main():
             continue
 
     f.close()
-
-
-def get_correct_release_asset(assets):
-    for a in assets:
-        if "windows" in a.name:
-            return a.browser_download_url
-
-
-def find_and_replace_templates(directory, version, tag, url, checksum):
-    os.mkdir(Path(directory)/"tools")
-    d = dict(version=version, tag=tag, url=url, checksum=checksum)
-    basepath = Path(os.getcwd())/"src/templates/rust-analyzer/"
-    templates = [
-            "rust-analyzer.nuspec",
-            "tools/chocolateyinstall.ps1"]
-
-    for template in templates:
-        with io.open(basepath/template, "r") as f:
-            contents = f.read()
-            temp = Template(contents).safe_substitute(d)
-            f = io.open(Path(directory)/template, "a+")
-            f.write(temp)
-            f.close()
-    return
-
-
-def abort_on_nonzero(retcode):
-    if (retcode != 0):
-        os.exit(retcode)
 
 
 if __name__ == "__main__":
