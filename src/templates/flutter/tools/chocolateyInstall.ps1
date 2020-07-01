f7ed68bf6024a1f1c677fd98c1239134e375eddd
Starz0r <starz0r@starz0r.com>
2020-07-01 00:36:56 -0500

    flutter: add bin folder to path
    Resolves #1

--- src/templates/flutter/tools/chocolateyInstall.ps1
+++ src/templates/flutter/tools/chocolateyInstall.ps1
@@ -3,4 +3,4 @@ $ToolsPath             = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $installDir            = $env:ChocolateyToolsLocation # Dangerous but appending -SpecificFolder to Install-ChocolateyZipPackage doesn't work
 
 Install-ChocolateyZipPackage $env:ChocolateyPackageName "$url" $installDir -checksum '$checksum' -checksumType 'sha256'
-Install-ChocolateyPath $installDir
+Install-ChocolateyPath "$installDir\bin"
