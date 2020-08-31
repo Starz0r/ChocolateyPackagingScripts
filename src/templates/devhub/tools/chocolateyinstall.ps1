41c3782e81b657d66d1ac0b48ae3407ed8fd7c8e
Starz0r <starz0r@starz0r.com>
2020-08-31 03:45:46 -0500

    devhub: nupkg template

---
+++ src/templates/devhub/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$PackageName  = $env:ChocolateyPackageName
+$App = $PackageName.Split(".")[0]
+$InstallDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$PackageArgs = @{
+  PackageName    = $PackageName
+  UnzipLocation  = $InstallDir
+  FileType       = 'exe'
+  Url64          = "$url"
+
+  SoftwareName   = 'devhub*'
+
+  Checksum64     = "$checksum"
+  ChecksumType64 = 'sha512'
+  
+  SilentArgs     = '/S /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  ValidExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @PackageArgs
