27f7ddf9c62b3cd7095a9ae7b54ce38c300be719
Starz0r <starz0r@starz0r.com>
2020-09-10 12:59:51 -0500

    devhub: legal documents

--- src/templates/devhub/tools/chocolateyinstall.ps1
+++ src/templates/devhub/tools/chocolateyinstall.ps1
@@ -3,20 +3,21 @@
 $PackageName  = $env:ChocolateyPackageName
 $App = $PackageName.Split(".")[0]
 $InstallDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ToolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $PackageArgs = @{
-  PackageName    = $PackageName
-  UnzipLocation  = $InstallDir
-  FileType       = 'exe'
-  Url64          = "$url"
+  PackageName     = $PackageName
+  UnzipLocation   = $InstallDir
+  FileType        = 'exe'
+  File64          = Get-Item $(Join-Path $ToolsDir "$fname")
 
-  SoftwareName   = 'devhub*'
+  SoftwareName    = 'devhub*'
 
-  Checksum64     = "$checksum"
-  ChecksumType64 = 'sha512'
+  Checksum64      = "$checksum"
+  ChecksumType64  = 'sha512'
   
-  SilentArgs     = '/S /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  ValidExitCodes = @(0)
+  SilentArgs      = "/S", "/SILENT", "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART", "/SP-", "/LOG"
+  ValidExitCodes  = @(0)
 }
 
 Install-ChocolateyPackage @PackageArgs
