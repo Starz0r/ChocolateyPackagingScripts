2c279655e9365b0f1127c5a247c03f6b53c805ea
Starz0r <starz0r@starz0r.com>
2020-09-10 13:03:12 -0500

    gallery-dl: legal documents and removal of the install script

--- src/templates/gallery-dl/tools/chocolateyinstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$ErrorActionPreference = "Stop";
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-Get-ChocolateyWebFile -PackageName "gallery-dl" -FileFullPath "$installDir\gallery-dl.exe" -Url "$url" -Checksum "$checksum" -ChecksumType "SHA512"
