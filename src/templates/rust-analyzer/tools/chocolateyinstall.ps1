d20a252faf749730eb0658a369d923f5bb7a5b0b
Starz0r <starz0r@starz0r.com>
2020-09-11 00:07:16 -0500

    rust-analyzer: legal documents and removal of the install script

--- src/templates/rust-analyzer/tools/chocolateyinstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$ErrorActionPreference = "Stop";
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-Get-ChocolateyWebFile -PackageName "rust-analyzer" -FileFullPath "$installDir\rust-analyzer.exe" -Url64bit "$url" -Checksum64 "$checksum" -ChecksumType64 "SHA512"
