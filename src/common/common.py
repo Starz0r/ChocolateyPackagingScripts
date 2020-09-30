84761e2c6a4588c88f447f39def1bef1a1117eba
Starz0r <starz0r@starz0r.com>
2020-09-30 01:42:04 -0500

    common: allow a Dict to be directly passed to the templating function

--- src/common/common.py
+++ src/common/common.py
@@ -2,7 +2,7 @@ from github.PaginatedList import PaginatedList
 from github.GitReleaseAsset import GitReleaseAsset
 from github import Github
 
-from typing import Optional
+from typing import Optional, Dict
 from pathlib import Path
 import os
 import sys
@@ -10,9 +10,9 @@ import io
 from string import Template
 
 
-def get_correct_release_asset(assets: PaginatedList,
-                              include: str,
-                              exclude: Optional[str]) -> Optional[GitReleaseAsset]:
+def get_correct_release_asset(
+        assets: PaginatedList, include: str,
+        exclude: Optional[str]) -> Optional[GitReleaseAsset]:
     if exclude is None:
         for asset in assets:
             if (include in asset.name):
@@ -24,19 +24,14 @@ def get_correct_release_asset(assets: PaginatedList,
     return None
 
 
-def find_and_replace_templates(package_name: str,
-                               directory: str,
-                               version: str,
-                               tag: str,
-                               url: str,
-                               checksum: str,
-                               fname: Optional[str],
-                               url64: Optional[str],
+def find_and_replace_templates(package_name: str, directory: str, version: str,
+                               tag: str, url: str, checksum: str,
+                               fname: Optional[str], url64: Optional[str],
                                checksum64: Optional[str],
                                fname64: Optional[str],
                                notes: Optional[str]) -> None:
-    os.mkdir(Path(directory)/"tools")
-    os.mkdir(Path(directory)/"legal")
+    os.mkdir(Path(directory) / "tools")
+    os.mkdir(Path(directory) / "legal")
     d = dict(version=version,
              tag=tag,
              url=url,
@@ -46,24 +41,54 @@ def find_and_replace_templates(package_name: str,
              checksum64=checksum64,
              fname64=fname64,
              notes=notes)
-    basepath = Path(os.getcwd())/"src/templates/"/package_name
+    basepath = Path(os.getcwd()) / "src/templates/" / package_name
     templates = [
-        package_name+".nuspec",
-        "tools/chocolateyinstall.ps1",
-        "tools/chocolateyuninstall.ps1",
-        "tools/chocolateybeforemodify.ps1",
-        "legal/LICENSE.txt",
-        "legal/VERIFICATION.txt",
-        "tools/LICENSE.txt",
-        "tools/VERIFICATION.txt"]
+        package_name + ".nuspec", "tools/chocolateyinstall.ps1",
+        "tools/chocolateyuninstall.ps1", "tools/chocolateybeforemodify.ps1",
+        "legal/LICENSE.txt", "legal/VERIFICATION.txt", "tools/LICENSE.txt",
+        "tools/VERIFICATION.txt"
+    ]
 
     for template in templates:
+        print(template)
         try:
-            with io.open(basepath/template, "r", encoding="utf-8-sig") as f:
+            with io.open(basepath / template, "r", encoding="utf-8-sig") as f:
                 contents = f.read()
                 temp = Template(contents).safe_substitute(d)
-                f = io.open(Path(directory)/template, "a+",
-                            encoding="utf-8", errors="ignore")
+                f = io.open(Path(directory) / template,
+                            "a+",
+                            encoding="utf-8",
+                            errors="ignore")
+                f.write(temp)
+                f.close()
+        except FileNotFoundError as err:
+            print("Could not find file to be templated.")
+            print(err)
+    return
+
+
+def find_and_replace_templates_new(package_name: str, directory: str,
+                                   d: Dict[str, str]) -> None:
+    os.mkdir(Path(directory) / "tools")
+    os.mkdir(Path(directory) / "legal")
+    basepath = Path(os.getcwd()) / "src/templates/" / package_name
+    templates = [
+        package_name + ".nuspec", "tools/chocolateyinstall.ps1",
+        "tools/chocolateyuninstall.ps1", "tools/chocolateybeforemodify.ps1",
+        "legal/LICENSE.txt", "legal/VERIFICATION.txt", "tools/LICENSE.txt",
+        "tools/VERIFICATION.txt"
+    ]
+
+    for template in templates:
+        print(template)
+        try:
+            with io.open(basepath / template, "r", encoding="utf-8-sig") as f:
+                contents = f.read()
+                temp = Template(contents).safe_substitute(d)
+                f = io.open(Path(directory) / template,
+                            "a+",
+                            encoding="utf-8",
+                            errors="ignore")
                 f.write(temp)
                 f.close()
         except FileNotFoundError as err:
