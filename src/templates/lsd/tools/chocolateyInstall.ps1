135951450f3f2c9f0968dced5b96b2723dda3619
Starz0r <starz0r@starz0r.com>
2020-09-30 21:49:05 -0500

    lsd: template and legal documents

---
+++ src/templates/lsd/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+$PackageName = 'lsd';
+$ToolsPath   = Split-Path -Parent $MyInvocation.MyCommand.Definition;
+$PkgParams   = Get-PackageParameters;
+
+if ($PkgParams.GNU) {
+	$PackageArgs = @{
+		PackageName    = $PackageName
+		FileFullPath   = Get-Item $(Join-Path $ToolsPath '$altfname')
+		FileFullPath64 = Get-Item $(Join-Path $ToolsPath '$altfname64')
+		Destination    = $ToolsPath
+	};
+} else {
+	$PackageArgs = @{
+		PackageName    = $PackageName
+		FileFullPath   = Get-Item $(Join-Path $ToolsPath '$fname')
+		FileFullPath64 = Get-Item $(Join-Path $ToolsPath '$fname64')
+		Destination    = $ToolsPath
+	};
+};
+
+Get-ChocolateyUnzip @PackageArgs;
+Remove-Item ($ToolsPath + '\*.' + 'zip');
