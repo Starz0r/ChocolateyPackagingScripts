bdeee5d8a9023eb8c526a86c5a35464bf713dbe3
Starz0r <starz0r@starz0r.com>
2020-10-14 03:20:55 -0500

    led: templates and legal documents

---
+++ src/templates/led/tools/ChocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$PackageName           = $env:ChocolateyPackageName;
+$ToolsDir              = Split-Path -parent $MyInvocation.MyCommand.Definition;
+
+$PackageArgs = @{
+  PackageName     = $PackageName
+  
+  FileType        = 'exe'
+  File64          = Get-Item $(Join-Path $ToolsDir "$fname")
+
+  SoftwareName    = 'led*'
+  
+  SilentArgs      = "/S"
+  ValidExitCodes  = @(0,3010)
+};
+
+Install-ChocolateyInstallPackage @PackageArgs;
+Remove-Item $(Get-Item $(Join-Path $ToolsDir "$fname"));
---
+++ src/templates/led/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,25 @@
+$ErrorActionPreference = 'Stop';
+$PackageName           = $env:ChocolateyPackageName;
+
+[array] $key = Get-UninstallRegistryKey "PackageName*";
+if ($key.Count -eq 1) {
+    $key | ForEach-Object {
+        $PackageArgs = @{
+            PackageName            = $PackageName
+            SilentArgs             = '/S'
+            FileType               = 'EXE'
+            ValidExitCodes         = @(0,3010)
+            File                   = "$($_.UninstallString.Replace(' /x86=0', ''))"
+        };
+        Uninstall-ChocolateyPackage @PackageArgs;
+    };
+}
+elseif ($key.Count -eq 0) {
+    Write-Warning "$PackageName has already been uninstalled by other means.";
+}
+elseif ($key.Count -gt 1) {
+    Write-Warning "$($key.Count) matches found!";
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled.";
+    Write-Warning "Please alert package maintainer the following keys were matched:";
+    $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"};
+};
