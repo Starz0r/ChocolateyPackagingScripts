a639fb06558ee06587a54371e87e3e97aa16b7fb
Starz0r <starz0r@starz0r.com>
2020-10-14 03:39:28 -0500

    emulsion: templates and legal documents

---
+++ src/templates/emulsion/tools/ChocolateyInstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop';
+$PackageName           = $env:ChocolateyPackageName;
+$ToolsDir              = Split-Path -parent $MyInvocation.MyCommand.Definition;
+$PkgParams             = Get-PackageParameters;
+$Desktop               = [System.Environment]::GetFolderPath("Desktop");
+$PrgmFiles             = [System.Environment]::GetFolderPath("ProgramFiles");
+
+$PackageArgs = @{
+  PackageName     = $PackageName
+  
+  FileType        = 'exe'
+  File64          = Get-Item $(Join-Path $ToolsDir "$fname")
+
+  SoftwareName    = 'emulsion*'
+  
+  SilentArgs      = "/S"
+  ValidExitCodes  = @(0,3010)
+};
+
+Install-ChocolateyInstallPackage @PackageArgs;
+
+if ($PkgParams.DesktopShortcut)
+{
+	Install-ChocolateyShortcut -ShortcutFilePath "$Desktop\Emulsion.lnk" -TargetPath "$PrgmFiles\Emulsion\Emulsion.exe" -WorkingDirectory "$PrgmFiles\Emulsion" -WindowStyle 1
+}
+
+if ($PkgParams.AddToPath)
+{
+	Install-ChocolateyPath -PathToInstall "$PrgmFiles\Emulsion" -PathType 'Machine'
+}
+
+Remove-Item $(Get-Item $(Join-Path $ToolsDir "$fname"));
