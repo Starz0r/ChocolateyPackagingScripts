cd202511453bb6b57a6c12f1decb723b44f7fa34
Starz0r <starz0r@starz0r.com>
2020-10-14 02:55:04 -0500

    ludusavi: templates and legal documents

---
+++ src/templates/ludusavi/tools/ChocolateyInstall.ps1
@@ -0,0 +1,22 @@
+$PackageName = 'ludusavi';
+$ToolsPath   = Split-Path -Parent $MyInvocation.MyCommand.Definition;
+$PkgParams   = Get-PackageParameters;
+
+if ($PkgParams.OpenGL) {
+	$PackageArgs = @{
+		PackageName    = $PackageName
+		FileFullPath   = Get-Item $(Join-Path $ToolsPath '$altfname')
+		FileFullPath64 = Get-Item $(Join-Path $ToolsPath '$altfname64')
+		Destination    = $ToolsPath
+	};
+} else {
+	$PackageArgs = @{
+		PackageName    = $PackageName
+		FileFullPath   = Get-Item $(Join-Path $ToolsPath '$fname')
+		FileFullPath64 = Get-Item $(Join-Path $ToolsPath '$fname64')
+		Destination    = $ToolsPath
+	};
+};
+
+Get-ChocolateyUnzip @PackageArgs;
+Remove-Item ($ToolsPath + '\*.' + 'zip');
