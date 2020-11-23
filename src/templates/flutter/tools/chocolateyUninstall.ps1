5c81378e9de85fa4adb9039d72df2c65556fe56e
Felipe Santos <felipecassiors@gmail.com>
2020-11-23 13:06:56 +0000

    Fix flutter bin folder on path

--- src/templates/flutter/tools/chocolateyUninstall.ps1
+++ src/templates/flutter/tools/chocolateyUninstall.ps1
@@ -3,5 +3,5 @@ $installDir = Join-Path $env:ChocolateyToolsLocation "flutter"
 Remove-Item -Path $installDir -Force -Recurse
 
 $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-$updatedPath = $oldpath.Replace(";$installDir", '')
+$updatedPath = ($oldpath.Split(';') | Where-Object { $_ -ne "$installDir\bin" }) -join ';'
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $updatedPath
