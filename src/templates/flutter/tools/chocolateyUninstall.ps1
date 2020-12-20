8001fb12f78c4a7e2fb62f0a9b3b408844147e38
Starz0r <starz0r@starz0r.com>
2020-12-19 20:01:10 -0600

    flutter: don't try to remove the variable from PATH when uninstalling
    Rejects #3

--- src/templates/flutter/tools/chocolateyUninstall.ps1
+++ src/templates/flutter/tools/chocolateyUninstall.ps1
@@ -1,7 +1,3 @@
-$installDir = Join-Path $env:ChocolateyToolsLocation "flutter"
+$InstallDir = Join-Path $env:ChocolateyToolsLocation "flutter";
 
-Remove-Item -Path $installDir -Force -Recurse
-
-$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-$updatedPath = $oldpath.Replace(";$installDir", '')
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $updatedPath
+Remove-Item -Path $InstallDir -Force -Recurse;
