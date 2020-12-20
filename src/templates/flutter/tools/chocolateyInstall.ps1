98c29ce3adfffa7cdc8395bb56fa6142fe50ba68
Starz0r <starz0r@starz0r.com>
2020-12-19 19:50:59 -0600

    flutter: insert correct directory to PATH
    Closes #2

--- src/templates/flutter/tools/chocolateyInstall.ps1
+++ src/templates/flutter/tools/chocolateyInstall.ps1
@@ -3,4 +3,4 @@ $ToolsPath             = Split-Path -Parent $MyInvocation.MyCommand.Definition;
 $InstallDir            = Get-ToolsLocation; # Dangerous but appending -SpecificFolder to Install-ChocolateyZipPackage doesn't work
 
 Install-ChocolateyZipPackage $env:ChocolateyPackageName "$url" $InstallDir -checksum '$checksum' -checksumType 'sha256';
-Install-ChocolateyPath "$InstallDir\bin";
+Install-ChocolateyPath "$InstallDir\flutter\bin";
