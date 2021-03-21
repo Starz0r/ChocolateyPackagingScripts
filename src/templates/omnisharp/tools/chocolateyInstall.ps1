dd5b9d1b5981502be383b64bb10f08bb940a388a
Starz0r <starz0r@starz0r.com>
2021-03-21 15:27:25 -0500

    omnisharp: update paths for ignored executable shims
    Closes https://github.com/Starz0r/ChocolateyPackagingScripts/issues/4.

--- src/templates/omnisharp/tools/chocolateyInstall.ps1
+++ src/templates/omnisharp/tools/chocolateyInstall.ps1
@@ -7,18 +7,13 @@ $PackageArgs = @{
 	Destination = $ToolsPath
 }
 Get-ChildItem $ToolsPath\* | Where-Object { $_.PSISContainer } | Remove-Item -Recurse -Force #remove older package dirs
-New-Item -Type Directory $ToolsPath\MSBuild\ -EA 0
-New-Item -ItemType file $ToolsPath\MSBuild\MSBuild.exe.ignore -EA 0
-New-Item -Type Directory $ToolsPath\MSBuild\Roslyn\ -EA 0
-New-Item -ItemType file $ToolsPath\MSBuild\Roslyn\csc.exe.ignore -EA 0
-New-Item -ItemType file $ToolsPath\MSBuild\Roslyn\csi.exe.ignore -EA 0
-New-Item -Type Directory $ToolsPath\refs\ -EA 0
-New-Item -ItemType file $ToolsPath\refs\OmniSharp.exe.ignore -EA 0
-New-Item -Type Directory $ToolsPath\.msbuild\ -EA 0
-New-Item -Type Directory $ToolsPath\.msbuild\Current\ -EA 0
+
 New-Item -Type Directory $ToolsPath\.msbuild\Current\Bin\ -EA 0
+New-Item -ItemType file $ToolsPath\.msbuild\Current\Bin\MSBuild.exe.ignore -EA 0
+
 New-Item -Type Directory $ToolsPath\.msbuild\Current\Bin\Roslyn\ -EA 0
 New-Item -ItemType file $ToolsPath\.msbuild\Current\Bin\Roslyn\csc.exe.ignore -EA 0
 New-Item -ItemType file $ToolsPath\.msbuild\Current\Bin\Roslyn\csi.exe.ignore -EA 0
+
 Get-ChocolateyUnzip @PackageArgs
 Remove-Item $ToolsPath\*.zip -ea 0
