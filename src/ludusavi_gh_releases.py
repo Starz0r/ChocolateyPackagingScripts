b4d33f14368394bdbc55ca7047073ed1f7287ab6
Starz0r <starz0r@starz0r.com>
2021-08-24 18:25:13 -0500

    ludusavi: opengl is default, upstream directx support was dropped

--- src/ludusavi_gh_releases.py
+++ src/ludusavi_gh_releases.py
@@ -15,39 +15,16 @@ def main():
     logger = logging.getLogger('Ludusavi GitHub Releases')
     logger.setLevel(logging.DEBUG)
 
-    looped_back = False
-
-    asset = None
-    asset64 = None
-    altasset = None
-    altasset64 = None
-
     for rel in on_new_git_release("ludusavi", "mtkennerly/ludusavi"):
         # correlate assets
-        if asset is None:
-            asset = get_correct_release_asset(
+        asset = get_correct_release_asset(
                 rel.get_assets(), "win32.zip",
-                "opengl")
-        if asset64 is None:
-            asset64 = get_correct_release_asset(
+                None)
+        asset64 = get_correct_release_asset(
                 rel.get_assets(), "win64.zip",
-                "opengl")
-        if altasset is None:
-            altasset = get_correct_release_asset(
-                rel.get_assets(), "+opengl-win32", "linux")
-        if altasset64 is None:
-            altasset64 = get_correct_release_asset(
-                rel.get_assets(), "+opengl-win64.zip", "linux")
+                None)
 
-        if looped_back is False:
-            print(asset)
-            print(asset64)
-            print(altasset)
-            print(altasset64)
-            looped_back = True
-            continue
-
-        if asset is None or asset64 is None or altasset is None or altasset64 is None:
+        if asset is None or asset64 is None:
             logger.warn(
                 'Versioned release missing required assets, therefore, ineligible for packaging, skipping.'
             )
@@ -65,19 +42,6 @@ def main():
         abort_on_nonzero(
             subprocess.call(["wget", url64, "--output-document", fname64]))
         chksum64 = checksum.get_for_file(fname64, "sha512")
-
-        alturl = altasset.browser_download_url
-        altfname = altasset.name
-        abort_on_nonzero(
-            subprocess.call(["wget", alturl, "--output-document", altfname]))
-        altchksum = checksum.get_for_file(altfname, "sha512")
-
-        alturl64 = altasset64.browser_download_url
-        altfname64 = altasset64.name
-        abort_on_nonzero(
-            subprocess.call(
-                ["wget", alturl64, "--output-document", altfname64]))
-        altchksum64 = checksum.get_for_file(altfname64, "sha512")
 
         # assemble information
         relnotes = rel.body
@@ -98,12 +62,6 @@ def main():
                  url64=url64,
                  checksum64=chksum64,
                  fname64=fname64,
-                 alturl=alturl,
-                 altchecksum=altchksum,
-                 altfname=altfname,
-                 alturl64=alturl64,
-                 altchecksum64=altchksum64,
-                 altfname64=altfname64,
                  notes=relnotes)
 
         # template and pack
@@ -111,19 +69,10 @@ def main():
         find_and_replace_templates_new("ludusavi", tmpdir, d)
         os.rename(fname, os.path.join(tmpdir, "tools", fname))
         os.rename(fname64, os.path.join(tmpdir, "tools", fname64))
-        os.rename(altfname, os.path.join(tmpdir, "tools", altfname))
-        os.rename(altfname64, os.path.join(tmpdir, "tools", altfname64))
         abort_on_nonzero(
             subprocess.call(
                 ["choco", "pack",
                  Path(tmpdir) / "ludusavi.nuspec"]))
-
-        # reset variables
-        looped_back = False
-        asset = None
-        asset64 = None
-        altasset = None
-        altasset64 = None
 
 
 if __name__ == "__main__":
--- src/templates/ludusavi/tools/ChocolateyInstall.ps1
+++ src/templates/ludusavi/tools/ChocolateyInstall.ps1
@@ -2,20 +2,11 @@ $PackageName = 'ludusavi';
 $ToolsPath   = Split-Path -Parent $MyInvocation.MyCommand.Definition;
 $PkgParams   = Get-PackageParameters;
 
-if ($PkgParams.OpenGL) {
-	$PackageArgs = @{
-		PackageName    = $PackageName
-		FileFullPath   = Get-Item $(Join-Path $ToolsPath '$altfname')
-		FileFullPath64 = Get-Item $(Join-Path $ToolsPath '$altfname64')
-		Destination    = $ToolsPath
-	};
-} else {
-	$PackageArgs = @{
+$PackageArgs = @{
 		PackageName    = $PackageName
 		FileFullPath   = Get-Item $(Join-Path $ToolsPath '$fname')
 		FileFullPath64 = Get-Item $(Join-Path $ToolsPath '$fname64')
 		Destination    = $ToolsPath
-	};
 };
 
 Get-ChocolateyUnzip @PackageArgs;
