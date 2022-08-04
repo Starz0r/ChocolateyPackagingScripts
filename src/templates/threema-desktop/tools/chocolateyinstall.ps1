7e6118f02b18f5c98c84116c78639f5583e9aa3e
Starz0r <starz0r@starz0r.com>
2022-08-03 22:38:24 -0500

    threema-desktop: package and distribute, mostly a temporary setup

---
+++ src/templates/threema-desktop/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$ToolsDir               = Split-Path $MyInvocation.MyCommand.Definition;
+
+$PackageArgs = @{
+  file64         = Join-Path $ToolsDir '$fname'
+  packageName    = $env:ChocolateyPackageName
+  installerType  = 'exe'
+  silentArgs     = '--silent'
+  validExitCodes = @(0)
+  softwareName   = 'Threema' 
+};
+
+Install-ChocolateyInstallPackage @PackageArgs;
+
+Remove-Item -ea 0 -Force -Path $ToolsDir\*.exe;
