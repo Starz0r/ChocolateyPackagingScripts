900eccc0a5e5e9ffb4998b856976023e84f7df6a
Starz0r <starz0r@starz0r.com>
2023-03-27 00:20:14 -0500

    common/events: allow git tags to be iterated through

--- src/common/events.py
+++ src/common/events.py
@@ -1,11 +1,14 @@
+from typing import Generator
 from github import Github
 from github.GitRelease import GitRelease
+from github.Tag import Tag
 
 import os
 import io
 
+gh = Github(os.environ['GH_TOKEN'])
 
-def on_new_git_release(pkg: str, repo_name: str) -> GitRelease:
+def on_new_git_release(pkg: str, repo_name: str) -> Generator[GitRelease, None, None]:
     # initalize state array
     state = []
     with io.open(pkg + ".ghstate", "a+") as f:
@@ -15,7 +18,6 @@ def on_new_git_release(pkg: str, repo_name: str) -> GitRelease:
     f = io.open(pkg + ".ghstate", "a+")
 
     # connect to github api
-    gh = Github(os.environ['GH_TOKEN'])
     repo = gh.get_repo(repo_name)
 
     # collect unpushed git releases
@@ -30,4 +32,30 @@ def on_new_git_release(pkg: str, repo_name: str) -> GitRelease:
         if pushed is False:
             yield rel
             f.write(str(rel.id) + "\n")
+            f.flush()
+
+def on_each_git_tag(pkg: str, repo_name: str) -> Generator[Tag, None, None]:
+     # initalize state array
+    state = []
+    with io.open(pkg + ".ghtstate", "a+") as f:
+        f.seek(0)  # why I have to do this is unknown
+        for _, line in enumerate(f):
+            state.append(line)
+    f = io.open(pkg + ".ghtstate", "a+")
+
+    # connect to github api
+    repo = gh.get_repo(repo_name)
+
+    # collect unpushed git tags
+    for tag in repo.get_tags():
+        pushed = False
+        for i in range(len(state)):
+            if str(state[i]).replace("\n", "") == str(tag.name):
+                pushed = True
+                break
+            else:
+                continue
+        if pushed is False:
+            yield tag
+            f.write(str(tag.name) + '\n')
             f.flush()
